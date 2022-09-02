5709054e43bc5f3e04df74e4006d699a49d33b50
Anvar571 <abdurashidovanvar52@gmail.com>
2022-09-02 11:04:42 +0500

    list and get function created todo

--- cmd/to-do/main.go
+++ cmd/to-do/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
-	"time"
 	"strconv"
-	"errors"
+	"time"
 )
 
 type ToDoStatus int
@@ -18,29 +18,32 @@ const (
 )
 
 type Todo struct {
-	ID int
-	Title string
+	ID          int
+	Title       string
 	Discription string
-	CreatedAt time.Time
-	Status ToDoStatus
+	CreatedAt   time.Time
+	Status      ToDoStatus
 }
 
-var todoList = []Todo{
-	{
-		ID:		1,
-		Title:   "my bootcamp course",
-		Discription: "learning go programming",
-		CreatedAt: time.Now(),
-		Status: StatusToDo,
-	},
-	{
-		ID:		2,
-		Title:   "my bootcamp my first app",
-		Discription: "learning go programming",
-		CreatedAt: time.Now(),
-		Status: StatusInProgres,
-	},
-}
+var (
+	todoList = []Todo{
+		{
+			ID:          1,
+			Title:       "my bootcamp course",
+			Discription: "learning go programming",
+			CreatedAt:   time.Now(),
+			Status:      StatusToDo,
+		},
+		{
+			ID:          2,
+			Title:       "my bootcamp my first app",
+			Discription: "learning go programming",
+			CreatedAt:   time.Now(),
+			Status:      StatusInProgres,
+		},
+	}
+	lastID int = 2
+)
 
 func List() []Todo {
 	return todoList
@@ -56,17 +59,52 @@ func Get(id int) (Todo, error) {
 	return Todo{}, error
 }
 
+func Create(title, discription string) Todo {
+	lastID++
+	newTodo := Todo{
+		ID:          lastID,
+		Title:       title,
+		Discription: discription,
+		CreatedAt:   time.Now(),
+		Status:      StatusToDo,
+	}
+
+	todoList = append(todoList, newTodo)
+
+	return newTodo
+}
+
+func Save() error {
+	f, err := os.OpenFile("./database.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	for _, todo := range todoList {
+		todoStr := fmt.Sprintf("%d;%s;%s;%s;%d\n", todo.ID, todo.Title, todo.Discription, todo.CreatedAt.Format(time.RFC3339), todo.Status)
+		
+		_, err := f.WriteString(todoStr)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	lenOsArgs:= len(os.Args)
+	lenOsArgs := len(os.Args)
 	if lenOsArgs < 2 {
 		fmt.Println("not enouph argumments")
 		os.Exit(1)
 	}
-
+	defer Save()
+	
 	switch os.Args[1] {
 	case "list":
 		todos := List()
-		for i:= range todos {
+		for i := range todos {
 			fmt.Printf("[%d] - %s %s %d\n", todos[i].ID, todos[i].Title, todos[i].Discription, todos[i].Status)
 		}
 	case "get":
@@ -83,6 +121,20 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("[%d] - %s %s %d\n", FoundTodo.ID, FoundTodo.Title, FoundTodo.Discription, FoundTodo.Status)
+	case "create":
+		var title, discription string
+
+		if lenOsArgs < 3 {
+			fmt.Println("can not argument create")
+			os.Exit(1)
+		}
+		title = os.Args[2]
+		if lenOsArgs == 4 {
+			discription = os.Args[3]
+		}
+
+		createdTodo := Create(title, discription)
+		fmt.Printf("[%d] - %s %s %d\n", createdTodo.ID, createdTodo.Title, createdTodo.Discription, createdTodo.Status)
 	default:
 		fmt.Println("invalid command argument")
 		os.Exit(1)
